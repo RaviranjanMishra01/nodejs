2a9eff9879a71fa6244f905b4fce7f5a9e734d85
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-01-26 15:51:06 +0530

    middleware and validation

---
+++ DAY-13-Middleware-ValidationError-Handling/middleware/validate.middleware.js
@@ -0,0 +1,11 @@
+module.exports = (req, res, next) => {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({
+            message: "Name and email are required"
+        });
+    }
+
+    next();
+};
